45c54bf39611dd464f52952d1e860dc21a8c8271
Piyush <piyush.dhamecha@gmail.com>
2024-09-16 13:08:01 +0530

    Updated project and skills

--- components/about.tsx
+++ components/about.tsx
@@ -25,8 +25,8 @@ export default function About() {
         className='mb-3'
       >
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">B.Com. (Accounting)</span>, I decided to pursue my
+        passion for programming. I finished my M.C.A. and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
@@ -35,14 +35,14 @@ export default function About() {
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with TypeScript. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my children. I also enjoy{" "}
+        table tennis, watching movies, and playing with my children. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>.
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import spreaddImage from "@/public/spreadd.png";
+import publicPoll from "@/public/public-poll.png";
+import finCast from "@/public/fin-cast.png";
+import weatherApp from "@/public/weather-app.png";
 
 export const links = [
   {
@@ -62,25 +63,32 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Spreadd",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Spreadd brings creators and brands closer together. I worked as a full-stack developer on this startup project for 1.5 years.",
+    tags: ["React", "Redux", "Node.js", "Express.js", "Graphql", "Postgresql"],
+    imageUrl: spreaddImage,
   },
   {
-    title: "rmtDev",
+    title: "Public Poll",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Next-gen digital democracy (Similar like social media). I worked as a full-stack developer on this startup project for 2 years.",
+    tags: ["React", "Redux", "Node.js", "Express.js", "Postgresql"],
+    imageUrl: publicPoll,
   },
   {
-    title: "Word Analytics",
+    title: "FinCast",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Financial forecasting mobile app. Predict the expenses upto 10 years based on the data entered by user.",
+    tags: ["React Native", "Firebase", "Node.js", "Express", "MongoDb"],
+    imageUrl: finCast,
+  },
+  {
+    title: "Weather App",
+    description:
+      "Detailed weather statistics and predictions. Includes wind, map, pollution, etc.",
+    tags: ["React", "Next.js", "Openweather api"],
+    imageUrl: weatherApp,
   },
 ] as const;
 
@@ -101,7 +109,5 @@ export const skillsData = [
   "Apollo",
   "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
